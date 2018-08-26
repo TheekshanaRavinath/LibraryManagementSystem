1d738df60d51060bb327ecc34e7c8ad03f744c44
ravikodithuwakku <ravinath.rmax@yahoo.com>
2018-08-26 20:04:12 +1000

    Member.java file is re modified according to reviewer opinions

--- member.java
+++ member.java
@@ -31,24 +31,24 @@ public class Member implements Serializable {//class name is modified by replaci
 
 	
 	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("Member:  ").append(ID).append("\n")
-		  .append("  Name:  ").append(LN).append(", ").append(FN).append("\n")
-		  .append("  Email: ").append(EM).append("\n")
-		  .append("  Phone: ").append(PN)
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("Member:  ").append(memberID).append("\n")
+		  .append("  Name:  ").append(loan).append(", ").append(FN).append("\n")
+		  .append("  Email: ").append(email).append("\n")
+		  .append("  Phone: ").append(contactNumber)
 		  .append("\n")
 		  .append(String.format("  Fines Owed :  $%.2f", FINES))
 		  .append("\n");
 		
-		for (loan loan : LNS.values()) {
-			sb.append(loan).append("\n");
+		for (loan loan : loans.values()) {
+			stringBuilder.append(loan).append("\n");
 		}		  
-		return sb.toString();
+		return stringBuilder.toString();
 	}
 
 	
 	public int getId() {
-		return ID;
+		return memberID;
 	}
 
 	
@@ -58,18 +58,18 @@ public class Member implements Serializable {//class name is modified by replaci
 
 	
 	public int getNumberOfCurrentLoans() {
-		return LNS.size();
+		return loans.size();
 	}
 
 	
 	public double getFinesOwed() {
-		return FINES;
+		return fines;
 	}
 
 	
 	public void takeOutLoan(loan loan) {
-		if (!LNS.containsKey(loan.getId())) {
-			LNS.put(loan.getId(), loan);
+		if (!loans.containsKey(loan.getId())) {
+			loans.put(loan.getId(), loan);
 		}
 		else {
 			throw new RuntimeException("Duplicate loan added to member");
@@ -78,12 +78,12 @@ public class Member implements Serializable {//class name is modified by replaci
 
 	
 	public String getLastName() {
-		return LN;
+		return lastName;
 	}
 
 	
 	public String getFirstName() {
-		return FN;
+		return firstName;
 	}
 
 
